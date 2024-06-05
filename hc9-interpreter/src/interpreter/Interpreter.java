2de77fbafc05fa97b7d5c8be2d43d558749fa637
maximquantum <maximquantum148@gmail.com>
2024-06-05 21:44:40 +0200

    dyn. subtyping: Addable, Andable

--- hc9-interpreter/src/interpreter/Interpreter.java
+++ hc9-interpreter/src/interpreter/Interpreter.java
@@ -2,19 +2,42 @@ package interpreter;
 
 abstract class Value {}
 
-class IntValue extends Value {
+abstract class AddableValue extends Value {
+	abstract Value add(Value other);
+}
+
+abstract class AndableValue extends Value {
+	abstract Value and(Value other);
+}
+
+class IntValue extends AddableValue { // , AndableValue <— solution (convert to interface instead of class)
 	int value;
 	IntValue(int value) { this.value = value; }
+	@Override
+	Value add(Value other) {
+		return new IntValue(value + ((IntValue)other).value);
+	}
+	
+	
 }
 
-class BooleanValue extends Value {
+class BooleanValue extends AndableValue {
 	boolean value;
 	BooleanValue(Boolean value) { this.value = value; }
+	@Override
+	Value and(Value other) {
+		return new BooleanValue(value & ((BooleanValue)other).value);
+	}
+	
 }
 
-class StringValue extends Value {
+class StringValue extends AddableValue {
 	String value;
 	StringValue(String value) { this.value = value; }
+	@Override
+	Value add(Value other) {
+		return new StringValue(value + ((StringValue)other).value);
+	}
 }
 
 public class Interpreter {
@@ -22,10 +45,8 @@ public class Interpreter {
 	static Value evaluate(Value value1, char operator, Value value2) {
 		switch (operator) {
 		case '+' -> {
-			if (value1 instanceof IntValue i)
-				return new IntValue(i.value + ((IntValue)value2).value);
-			else if (value1 instanceof StringValue s)
-				return new StringValue(s.value + ((StringValue)value2).value);
+			if (value1 instanceof AddableValue a)
+				return a.add(value2);
 			else
 				throw new RuntimeException("Type error");
 		}
